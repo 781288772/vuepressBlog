2f60f2cc1ffaf71fd291ccca0a1b9d0a64444add
这该死的魅力 <781288772@qq.com>
2021-11-23 16:03:29 +0800

    update

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -81,7 +81,7 @@ module.exports = {
     // 页脚信息
     createYear: 2020, // 博客创建年份
     copyrightInfo:
-      'Christina Chen | <a href="https://github.com/781288772/vuepressBlog/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      '<a href="https://beian.miit.gov.cn" target="_blank">粤ICP备2021157978号</a>|Christina Chen | <a href="https://github.com/781288772/vuepressBlog/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
   },
   // htmlModules // 插入hmtl(广告)模块
 }
